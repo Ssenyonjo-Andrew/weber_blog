738b6bc46bc61e583f927e5f53c9c1c20f65e739
Ssenyonjo-Andrew <ande.ssenyonjo@gmail.com>
2025-02-19 11:29:35 +0300

    intial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import { Josefin_Sans } from "next/font/google"
+
 export default function home(){
   
   return <div>
     <h1>data science for africa</h1>
     <h1>an initiative for 2025</h1>
       <p>welcome to our website</p>
+      <p>hope you are good</p>
       </div>
-
+    
 }
